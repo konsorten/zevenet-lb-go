47737a5b0bfae907756eb605efd0e12d797f8926
Felix Kollmann <fk@konsorten.de>
2018-11-19 14:25:12 +0100

    Add creating virtual interface for farm tests

--- zapi_farms_test.go
+++ zapi_farms_test.go
@@ -95,6 +95,15 @@ func TestRoundtripHTTPFarm(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	// create the new virtInt
+	vint, err := session.CreateVirtInt(unitTestVirtIntName, unitTestVirtualIP)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer session.DeleteVirtInt(vint.Name)
+
 	// create the new farm
 	farm, err := session.CreateFarmAsHTTP(unitTestFarmName, unitTestVirtualIP, 0)
 
@@ -254,6 +263,15 @@ func TestRoundtripHTTPSFarm(t *testing.T) {
 	certName := certs[0].Filename
 
 	t.Logf("Using certificate: %v", certName)
+
+	// create the new virtInt
+	vint, err := session.CreateVirtInt(unitTestVirtIntName, unitTestVirtualIP)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer session.DeleteVirtInt(vint.Name)
 
 	// create the new farm
 	farm, err := session.CreateFarmAsHTTPS(unitTestFarmName, unitTestVirtualIP, 0, certName)
